004483f6f1e5e25d5c0763e83e68d225234df630
Rémi Dupré <remi@dupre.io>
2025-06-09 10:42:17 +0200

    implement simple components from mockups

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,65 @@
-use opening_hours::OpeningHours;
+use opening_hours::localization::TzLocation;
+use opening_hours::{Context, OpeningHours};
 use yew::prelude::*;
 
 pub mod components {
-    pub mod normalized {
+    pub mod properties {
+        use chrono::{DateTime, Local};
         use opening_hours::OpeningHours;
+        use opening_hours::localization::TzLocation;
         use yew::prelude::*;
 
         #[derive(Properties, PartialEq)]
         pub struct Props {
-            pub oh: UseStateHandle<OpeningHours>,
+            pub raw_oh: UseStateHandle<String>,
+            pub oh: UseStateHandle<Option<OpeningHours<TzLocation<chrono_tz::Tz>>>>,
+            pub dt: UseStateHandle<DateTime<Local>>,
+            pub tz: UseStateHandle<chrono_tz::Tz>,
         }
 
         #[function_component]
-        pub fn Normalized(props: &Props) -> Html {
-            let stringified = props.oh.to_string();
-            let normalized = props.oh.normalize().to_string();
+        pub fn Properties(props: &Props) -> Html {
+            let Some(oh) = props.oh.as_ref() else {
+                return Html::default();
+            };
+
+            let dt = props.dt.with_timezone(&*props.tz);
+            let normalized = oh.normalize().to_string();
+            let is_normal = normalized == *props.raw_oh;
+            let state = oh.state(dt);
+            let next_change_opt = oh.next_change(dt);
 
             html! {
-              <p>
-                if normalized != stringified {
-                  <strong>{"Normalized:"}</strong> {normalized}
+              <>
+                <ul>
+                  <li><strong>{ "State: " }</strong> {state}</li>
+                  <li>
+                    <strong>{ "Next change: " }</strong>
+
+                    if let Some(next_change) = next_change_opt {
+                      {next_change}
+                    } else {
+                      { "never" }
+                    }
+                  </li>
+                  <li><strong>{ "Comment: " }</strong> { "hide if no comment" }</li>
+                  <li>
+                    if is_normal {
+                      <strong>{ "Already normalized" }</strong>
+                    }
+                    else {
+                      <strong>{ "Normalized: " }</strong>
+                      { normalized }
+                    }
+                  </li>
+                </ul>
+
+                if !is_normal {
+                  <div class="button-box">
+                    <button>{ "Apply Normalization" }</button>
+                  </div>
                 }
-              </p>
+              </>
             }
         }
     }
@@ -29,34 +67,65 @@ pub mod components {
 
 #[function_component]
 fn App() -> Html {
-    let oh = use_state(|| OpeningHours::parse("24/7").unwrap());
+    let raw_oh = use_state(String::new);
+    let oh = use_state(|| None);
+    let tz = use_state(|| chrono_tz::Europe::Paris);
+    let dt = use_state(chrono::Local::now);
 
     let oninput = {
+        let raw_oh_state = raw_oh.clone();
         let oh = oh.clone();
+        let tz = tz.clone();
 
         move |input: InputEvent| {
+            log::debug!("{input:?}");
+
             let raw_oh = input
-                .target_dyn_into::<web_sys::HtmlInputElement>()
+                .target_dyn_into::<web_sys::HtmlTextAreaElement>()
                 .map(|el| el.value())
                 .unwrap_or_default();
 
+            raw_oh_state.set(raw_oh.clone());
+
             match raw_oh.parse::<OpeningHours>() {
                 Ok(parsed_oh) => {
-                    log::debug!("New Expression: {}", *oh);
-                    oh.set(parsed_oh);
+                    let parsed_oh = parsed_oh
+                        .with_context(Context::default().with_locale(TzLocation::new(*tz)));
+
+                    log::debug!("New Expression: {}", parsed_oh);
+                    oh.set(Some(parsed_oh));
                 }
                 Err(err) => {
                     log::debug!("Invalid Expression for {raw_oh:?}: {err}");
+                    oh.set(None);
                 }
             }
         }
     };
 
+    let repo_url = "https://github.com/remi-dupre/opening-hours-rs/";
+
     html! {
-        <div>
-            <input {oninput} />
-            <components::normalized::Normalized oh={oh} />
-        </div>
+      <>
+        <section>
+          <textarea id="expression" {oninput}></textarea>
+        </section>
+        <section>
+          <components::properties::Properties raw_oh={raw_oh} oh={oh} dt={dt} tz={tz} />
+        </section>
+        <section>
+          <h2>{ "Open until 18:00" }</h2>
+        </section>
+        <section>
+          <h2>{ "What is this?" }</h2>
+          <p>
+            { "This is a demo website for the Rust & Python library " }
+            <a href={ repo_url }>{ "opening-hours-rs" }</a>
+            { ". You can find documentation [here] and [here]." }
+          </p>
+          <p>{ "The syntax is documented on the OpenStreetMap wiki [here]." }</p>
+        </section>
+      </>
     }
 }
 
